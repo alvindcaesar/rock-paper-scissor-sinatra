dccd92fec30c182de457766ed5ecb736052471a5
Alvind <me@alvindcaesar.com>
2021-04-22 12:37:56 +0800

    Added functionality

--- app.rb
+++ app.rb
@@ -1,105 +1,97 @@
 require 'sinatra'
 
-class RockPaperScissor < Sinatra::Base
+class RockPaperScissors < Sinatra::Base
 
   enable :sessions
 
   get '/' do
-     erb :homepage
+    erb :homepage
   end
 
-  post '/menu' do
-      session['player_name'] = params[:player_name]
-      @player_name = session['player_name'].capitalize!
-
-      erb :menu
+  post '/' do
+    session['player_name'] = params[:player_name].capitalize!
+    @player_name = session['player_name']
+    erb :rounds
   end
 
-  post '/play' do
-    session['rounds'] = params["rounds"]
-    @rounds = session['rounds'].to_i
-    erb :play
+  get '/menu' do
+    erb :menu
   end
 
-  get '/play' do
-    session['count'] = 0
-    session['count'] += 1
-    @rounds = session['rounds'].to_i
-    erb :play
+  post '/rounds' do
+    @player_name = session['player_name']
+    session['rounds'] = params[:rounds]
+    @rounds = params['rounds']
+    erb :menu
   end
 
-  get '/play/throw/:player_choice' do
+  get '/rounds' do
+    erb :rounds
+  end
 
-    options = %w(rock paper scissor)
-    machine_choice = rand(options.length)
+  get '/game' do
+    session['results'] = []
+    @count = session['rounds']
+    session['count'] = session['rounds'].to_i
+    erb :game
+  end
+
+  get '/game/throw/:player_choice' do
+
+    options = ['Rock','Paper','Scissors']
+    computer_choice = rand(options.length)
     player_choice = params[:player_choice]
+    @computer_answer = options[computer_choice]
+    session['count'] -= 1
 
-    @machine_answer = options[machine_choice]
-
-    # session['count'] += 1
     @count = session['count']
     @rounds = session['rounds'].to_i
 
-
-    if player_choice == 'rock' && @machine_answer == 'scissor'
-      #player wins
-      @result = 'You win!'
-      # store_results('win')
-    elsif player_choice == 'paper' && @machine_answer == 'rock'
-      #player wins
-      @result = 'You win!'
-      # store_results('win')
-    elsif player_choice == 'scissor' && @machine_answer == 'paper'
-      #player wins
-      @result = 'You win!'
-      # store_results('win')
-    elsif @machine_answer == 'rock' && player_choice == 'scissor'
-      #machine wins
-      @result = 'You lose!'
-      # store_results('lose')
-    elsif @machine_answer == 'paper' && player_choice == 'rock'
-      #machine wins
-      @result = 'You lose!'
-      # store_results('lose')
-    elsif @machine_answer == 'scissor' && player_choice == 'paper'
-      #machine wins
-      @result = 'You lose!'
-      # store_results('lose')
+    if player_choice == 'Rock' && @computer_answer == 'Scissors'
+      @result = 'Awesome, you win!'
+      store_results('win')
+    elsif player_choice == 'Paper' && @computer_answer == 'Rock'
+      @result = 'Awesome, you win!'
+      store_results('win')
+    elsif player_choice == 'Scissors' && @computer_answer == 'Paper'
+      @result = 'Awesome, you win!'
+      store_results('win')
+    elsif @computer_answer == 'Rock' && player_choice == 'Scissors'
+      @result = 'Sorry mate, you lose!'
+      store_results('lose')
+    elsif @computer_answer == 'Paper' && player_choice == 'Rock'
+      @result = 'Sorry mate, you lose!'
+      store_results('lose')
+    elsif @computer_answer == 'Scissors' && player_choice == 'Paper'
+      @result = 'Sorry mate, you lose!'
+      store_results('lose')
     else
-      #ties
-      @result = 'Tie!'
-      # store_results('ties')
+      @result = 'Ok cool, it is tie!'
+      store_results('ties')
     end
-
-    # session['count'] += 1
-
-    erb :play
+      erb :game
   end
 
   get '/results' do
-      session['final_results'] = []
-      @final_results = session['final_results']
-      @win_results = session['final_results'].count('win')
-      @lose_results = session['final_results'].count('lose')
-      @ties_results = session['final_results'].count('ties')
+    @player_name = session['player_name']
+      @results = session['results']
+      @win_results = session['results'].count('win')
+      @lose_results = session['results'].count('lose')
+      @ties_results = session['results'].count('ties')
 
-    erb :results
+      erb :results
+    end
+
+
+  def store_results(results)
+
+    case results
+    when 'win'
+      session['results'].push('win')
+    when 'lose'
+      session['results'].push('lose')
+    when 'ties'
+      session['results'].push('ties')
   end
-
-
-  # def store_results(final_results)
-  #
-  #   # case final_results
-  #   # when 'win'
-  #   #   session['final_results'].push('win')
-  #   # when 'lose'
-  #   #   session['final_results'].push('lose')
-  #   # when 'ties'
-  #   #   session['final_results'].push('ties')
-  # end
-
-  def store_multiple_results (name, results)
-    session['leaderboard'] = Hash.new
-    session['leaderboard'].store(name, results)
-  end
+end
 end
